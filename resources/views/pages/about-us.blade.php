e106e2936cd2204a1d5b3f1127a2b128f256626c
MattFrancia <MattFrancia@users.noreply.github.com>
2023-09-13 22:28:48 +0800

    I added timeline in About Us page

--- resources/views/pages/about-us.blade.php
+++ resources/views/pages/about-us.blade.php
@@ -7,8 +7,8 @@
     </x-sub-header-text>
 
 
-    <div class="container w-full px-5 py-20 mx-auto">
-      {{-- <div class="flex justify-center flex-wrap w-full gap-4"> --}}
+    {{-- <div class="container w-full px-5 py-20 mx-auto">
+      
          <h1>
           The funeral was established and started its operation on 2009,
           but our operations started as a hearse and van rental for huge funerals within the metro. 
@@ -22,21 +22,55 @@
          <p>
           We are located at 110 Bayanan Rd, Bacoor, 4102 Cavite
          </p>
-      {{-- </div> --}}
-  </div>
-  </section>
-  <section class="text-gray-600 body-font bg-gray-100 relative">
-    <div class="mx-auto max-w-[1240px]">
-        <div class="px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-            <div class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-                <iframe width="100%" height="100%" class="absolute inset-0" frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://maps.google.com/maps?q=Torres-Escaro Funeral Service 110 Bayanan Rd, Bacoor, 4102 Cavite&t=&z=17&ie=UTF8&iwloc=&output=embed"></iframe>
-                <div class="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-                    
-                    
-                </div>
-            </div>
+      
+  </div> --}}
+
+  <section class="bg-gray-100 text-black">
+    <div class="container max-w-5xl px-4 py-12 mx-auto">
+      <div class="grid gap-4 mx-4 sm:grid-cols-12">
+        <div class="col-span-12 sm:col-span-3">
+          <div class="text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:bg-violet-600">
+            <h3 class="text-3xl font-semibold">Our Story</h3>
+            <span class="text-sm font-bold tracki uppercase text-gray-600">Torres Escaro Funeral</span>
+          </div>
         </div>
+        <div class="relative col-span-12 px-4 space-y-6 sm:col-span-9">
+          <div class="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-gray-300">
+            <div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-violet-600">
+              <h3 class="text-xl font-semibold tracki">Torres Escaro Funeral services</h3>
+              <time class="text-xs tracki uppercase text-gray-600">2009</time>
+              <p class="mt-3">The funeral business is established and begins its operations.</p>
+            </div>
+            <div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-violet-600">
+              <h3 class="text-xl font-semibold tracki">Initial Years</h3>
+              <time class="text-xs tracki uppercase text-gray-600">2009</time>
+              <p class="mt-3">Our business starts by offering hearse and van rental services primarily for large funerals within the metro area. It begins with only 4 employees.</p>
+            </div>
+            <div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-violet-600">
+              <h3 class="text-xl font-semibold tracki">Continued Growth</h3>
+              <time class="text-xs tracki uppercase text-gray-600">2009</time>
+              <p class="mt-3">Over the years, the funeral business experiences continuous growth and expansion.</p>
+            </div>
+            <div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-violet-600">
+              <h3 class="text-xl font-semibold tracki">Services Diversification</h3>
+              <time class="text-xs tracki uppercase text-gray-600">2009</time>
+              <p class="mt-3">The funeral business expands its services to include memorial services, cremation services, as well as transportation and repatriation services.</p>
+            </div>
+            <div class="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-violet-600">
+              <h3 class="text-xl font-semibold tracki">Client Base</h3>
+              <time class="text-xs tracki uppercase text-gray-600">The funeral business caters to both local and international clients</time>
+              <p class="mt-3">Local Clients: Most local clients avail of the memorial services.</p>
+              <p class="mt-3">International Clients: International clients often utilize the transportation and repatriation services.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</section>
+  </section>
+  
+
+  </section>
+  
+
 
 </x-welcome-layout>
