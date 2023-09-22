c54f878943cb68a6a536e9d67c0a4376296b6f6f
CadsNvN <134202288+CadsNvN@users.noreply.github.com>
2023-09-22 13:39:14 +0800

    added AOS library

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,13 @@
 import './bootstrap';
 import Alpine from 'alpinejs';
 import 'boxicons';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
+AOS.init({
+    offset: 150,
+    duration: 1400
+});
 
 window.Alpine = Alpine;
 
