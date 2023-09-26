cca954e34e53002c1bb46e75c74e4407d9eb3037
CadsNvN <134202288+CadsNvN@users.noreply.github.com>
2023-09-26 13:28:53 +0800

    committing changes

---
+++ resources/views/receiptNew.blade.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title></title>
+		<!-- Mobile Specific -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- css -->
+		<link rel="stylesheet" type="text/css" href="css.css">
+	</head>
+	<body>
+		<div class="wrapper">
+<!-- 			<header class="header">
+				<img src="headerLogo.jpg" alt="headerLogo" class="imageheader">
+			</header> -->
+			<section class="subHeader">
+				<table >
+					<tr class="row">
+						<td class="col"colspan="2"><img src="headerLogo.jpg" alt="headerLogo" class="imageheader"></td>
+					</tr>
+					<tr class="row">
+						<td class="col"><h4>TO BE ACCOMPLISHED BY THE FAMILY</h4></td>
+						<td class="col">Series No.</td>
+					</tr>
+					<tr class="row">
+						<td class="col"><h4>PLEASE WRITE LEGIBLY</h4></td>
+						<td class="col"> SC No.</td>
+					</tr>
+					<tr>
+						<td colspan="2" align="center"><strong>INFORMATION SHEET</strong></td>
+					</tr>
+				</table>
+			</section>
+			<section class="deseaced">
+				<table>
+					<tr class="row">
+						<td class="col" colspan="4">Name:</td>
+					</tr>
+					<tr class="row">
+						<td class="col">Date of birth:</td>
+						<td class="col">Age:</td>
+						<td class="col">Height:</td>
+						<td class="col">Weight:</td>
+					</tr>
+					<tr class="row">
+						<td class="col" colspan="4">Address:</td>
+					</tr>
+					<tr class="row">
+						<td class="col">Occupation:</td>
+						<td class="col">Cizitenship:</td>
+						<td class="col">Religion:</td>
+					</tr>
+					<tr class="row">
+						<td class="col"colspan="2">Civil status:</td>
+						<td class="col"colspan="2">Sex:</td>
+					</tr>
+					<tr class="row">
+						<td class="col" colspan="2">Name of father:</td>
+						<td class="col" colspan="2">Maiden Name of Mother:</td>
+					</tr>
+					<tr class="row">
+						<td class="col">Cause of Death:</td>
+						<td class="col">Place of Death:</td>
+						<td class="col">Date of Death:</td>
+						<td class="col">Time of Death:_________AM/PM</td>
+					</tr>
+					<tr class="row">
+						<td class="col"colspan="4">Name & Address of Cementry:</td>
+					</tr>
+					<tr class="row">
+						<td class="col"colspan="4">Place of Viewing:</td>
+					</tr>
+					<tr class="row">
+						<td class="col"colspan="2">Date of Interment:</td>
+						<td class="col"colspan="2">Time of Interment:</td>
+					</tr>
+				</table>
+			</section>
+			<section class="informant">
+				<table>
+					<tr class="row">
+						<td class="col">Name of Informant:</td>
+						<td class="col">Age:</td>
+						<td class="col">Occupation:</td>
+					</tr>
+					<tr class="row">
+						<td class="col"colspan="3">Address:</td>
+					</tr>
+					<tr class="row">
+						<td class="col"colspan="1">Tel. No:</td>
+						<td class="col"colspan="2">Cell Phone. No:</td>
+					</tr>
+					<tr class="row">
+						<td class="col">Relationship to the deceased:</td>
+						<td class="col" colspan="2">Date:</td>
+					</tr>
+				</table>
+				<hr class="dotline">
+				<table>
+					<tr class="row">
+						<td class="col">Please accept our deepest condolences.</td>
+					</tr>
+					<tr class="row">
+						<td class="col">As it is our objective to ease your burden and assist you in this time of bereavement, and for us to provide you the best
+						service, may request you to take note of the following:</td>
+					</tr>
+					<tr class="row">
+						<td class="col">Please submit the death certificate of the deceased immediately for registration and securing of transfer/burial permit for interment. This is composed of three (3) to four (4) copies with a white copy for submission to National Statistics Office. (Memorial parks are requiring monetary bond for failure to present and submit burial permit. We would be constrained to ask you to post the necessary bond if you cannot submit to us the death certificate within reasonable period of time). Please notify us immediately of the date, time and place of interment or cremation, as the case may be or any schedule for necro logical service that you intend to do in honor of the deceased that needs assistance. We would appreciate it very much if you can provide us to right information as regards the claimant's accurate details to avoid repetitive corrections of the documents and settle your one (1) day before burial. This would relieve you of unnecessary concern and worry on the day of interment.</td>
+					</tr>
+				</table>
+			</section>
+			<section class="Employee">
+				<table>
+					<tr class="row">
+						<td class="col" colspan="2"><strong>TO BE ACCOMPLISHED BY FUNERAL REPRESNTATIVE:</strong></td>
+					</tr>
+					<tr class="row">
+						<td class="col">RB:</td>
+						<td class="col">:________________</td>
+					</tr>
+					<tr class="row">
+						<td class="col">Driver on Duty:</td>
+						<td class="col">:________________</td>
+					</tr>
+					<tr class="row">
+						<td class="col">Helper on Duty:</td>
+						<td class="col">:________________</td>
+					</tr>
+					<tr class="row">
+						<td class="col">Arrival Date & Date:</td>
+						<td class="col">:________________</td>
+					</tr>
+					<tr class="row">
+						<td class="col" colspan="2"><strong>Remarks:</strong></td>
+					</tr>
+					<tr class="row">
+						<td class="col" colspan="2">L:_________</td>
+					</tr>
+					<tr class="row">
+						<td class="col" colspan="2">W:_________</td>
+					</tr>
+					<tr class="row">
+						<td class="col" colspan="2">Page | 1</td>
+					</tr>
+				</table>
+			</section>
+			<section class="otherServices">
+				<table>
+					<tr class="row">
+						<td class="col"colspan="2"><img src="headerLogo.jpg" alt="headerLogo" class="imageheader"></td>
+					</tr>
+					<tr class="row">
+						<td class="col" colspan="2"><strong>AMOUNT OF SERVICE/S</strong></td>
+					</tr>
+					<tr class="row">
+						<td class="col">GROSS AMOUNT OF SERVICE:</td>
+						<td class="col">PHP</td>
+					</tr>
+					<tr class="row">
+						<td class="col">DISCOUNT(SERNIOR,PWD,OTHERS):</td>
+						<td class="col">PHP</td>
+					</tr>
+					<tr class="row">
+						<td class="col">GL(CSWD/DSWD):</td>
+						<td class="col">PHP</td>
+					</tr>
+					<tr class="row">
+						<td class="col">NET AMOUMT OF SERVICE:</td>
+						<td class="col">PHP</td>
+					</tr>
+					<tr class="row">
+						<td class="col" colspan="2"><strong>OTHER SERVICES:</strong></td>
+					</tr>
+					<tr class="row">
+						<td class="col">A.)</td>
+						<td class="col">PHP</td>
+					</tr>
+					<tr class="row">
+						<td class="col">B.)</td>
+						<td class="col">PHP</td>
+					</tr>
+					<tr class="row">
+						<td class="col">C.)</td>
+						<td class="col">PHP</td>
+					</tr>
+					<tr class="row">
+						<td class="col">GRAND TOTAL:</td>
+						<td class="col">PHP</td>
+					</tr>
+				</table>
+				<table>
+					<tr class="row">
+						<td class="col" align="center"><strong>PACKAGE INCLUSIONS</strong></td>
+					</tr>
+					<tr class="row">
+						<td class="col">Retrieval of Remains</td>
+					</tr>
+					<tr class="row">
+						<td class="col">Embalming</td>
+					</tr>
+					<tr class="row">
+						<td class="col">Casket</td>
+					</tr>
+					<tr class="row">
+						<td class="col">Viewing Equipment</td>
+					</tr>
+					<tr class="row">
+						<td class="col">Flower</td>
+					</tr>
+					<tr class="row">
+						<td class="col">Hearse For Interment</td>
+					</tr>
+					<tr class="row">
+						<td class="col">Hot & Cold-water Dispenser with ________ gallons of water</td>
+					</tr>
+					<tr class="row">
+						<td class="col">Facilitaion of Death Certificate and Permits c/o _______</td>
+					</tr>
+				</table>
+				<table>
+					<tr class="row">
+						<td class="col" align="center"><strong>PAYMENTS TERMS</strong></td>
+					</tr>
+					<tr class="row">
+						<td class="col"><strong>FOR MEMORIAL SERVICES:</strong> Down payment is optional not required, full payment for the memorial services should be settled <strong>1 Day before the interment</strong>.</td>
+					</tr>
+					<tr class="row">
+						<td class="col"><strong>FOR DIRECT CREMATION:</strong> Full payment is required <strong>before the scheduled time and date of cremation</strong>.</td>
+					</tr>
+					<tr class="row">
+						<td class="col">By filling out this document, i understand the above stated inforamtion and it has been properly discussed to me and i hereby agree to engage and avail the abovementioned memorial service. Witdrawal will be subjec for approval and changes.</td>
+					</tr>
+				</table>
+				<table>
+					<tr class="row">
+						<td class="col"colspan="2"><strong>CONFORME</strong></td>
+					</tr>
+					<tr class="row">
+						<td class="col">name</td>
+						<td class="col">date</td>
+					</tr>
+					<tr class="row">
+						<td class="col">SIGNATURE OVERPRINTED NAME OF CLIENT</td>
+						<td class="col">Date</td>
+					</tr>
+					<tr class="row">
+						<td class="col" colspan="2">Page | 2</td>
+					</tr>
+				</table>
+			</section>
+		</div>
+		<p>8.3 x 11.7 inches</p>
+		<p>2480 pixels x 3508 pixels</p>
+	</body>
+</html>
